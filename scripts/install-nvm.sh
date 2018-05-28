455d9af04647af845a4974e5ee73e60b8148baa4
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2018-05-28 10:27:02 -0300

    Improve nvm setup

--- scripts/install-nvm.sh
+++ scripts/install-nvm.sh
@@ -6,20 +6,22 @@ if ! [ -d ${NVM_DIR} ]; then
   git clone https://github.com/creationix/nvm.git "$NVM_DIR"
   cd "$NVM_DIR"
   git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
-
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 fi
 
-nvm use node
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
 if ! $(which node 2> /dev/null 1>&2); then
   # install default nodejs
   echo "Installing default nodejs"
-  nvm install
+  nvm install node
 fi
+
+nvm use node
 
 if ! $(which yarn 2> /dev/null 1>&2); then
   echo "Installing yarn"
   npm -g install yarn 
 fi
+
+yarn global add prettier
