0fd5427ef6663420fc852bcdaae08fd24f019995
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2018-05-28 14:00:26 -0300

    Refactor zsh environment setup

---
+++ zsh/sdkman.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="/home/sebas/.sdkman"
+[[ -s "/home/sebas/.sdkman/bin/sdkman-init.sh" ]] && \
+  source "/home/sebas/.sdkman/bin/sdkman-init.sh"
