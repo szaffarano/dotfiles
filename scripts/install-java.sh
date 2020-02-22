cb818a5e5ee185e8484215e051e068b170f6154a
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2020-02-22 15:14:39 -0300

    Update java versions

--- scripts/install-java.sh
+++ scripts/install-java.sh
@@ -1,6 +1,10 @@
 #!/bin/zsh
 
-JAVA_VER="adopt-openjdk-8u232-b09"
+declare -a JAVA_VERSIONS=(\
+    "adopt-openjdk-8u242-b08" \
+    "adopt-openjdk-11.0.6+10" \
+    "adopt-openjdk-13.0.2+8" \
+)
 
 source $HOME/.asdf/asdf.sh
 
@@ -9,11 +13,15 @@ if ! $(which asdf 2> /dev/null 1>&2); then
 	exit 1
 fi
 
-asdf plugin-add java
-asdf plugin-add maven
-asdf plugin-add gradle
+asdf plugin-add java || asdf plugin-update java
+asdf plugin-add maven || asdf plugin-update maven
+asdf plugin-add gradle || asdf plugin-update gradle
 
-asdf install java $JAVA_VER
+for JAVA_VER in "${JAVA_VERSIONS[@]}"
+do 
+    asdf install java $JAVA_VER
+done
+
 asdf install maven latest
 asdf install gradle latest
 
