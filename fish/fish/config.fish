344d54cc9d19228391ae4fd100719891bc9e57fb
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2020-07-19 12:26:24 -0300

    On demand local environment

--- fish/fish/config.fish
+++ fish/fish/config.fish
@@ -4,5 +4,4 @@ fish_vi_key_bindings
 
 if status --is-login
     [ -f ~/.config/fish/env.fish ]; and . ~/.config/fish/env.fish
-    [ -f ~/.config/fish/env.local.fish ]; and . ~/.config/fish/env.local.fish
 end
