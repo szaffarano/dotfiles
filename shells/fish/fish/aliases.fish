9c1ba815756e236637ef9b6be2aa8fd3bd60353b
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2020-09-24 10:26:28 -0300

    Use abbr instead of alias

--- shells/fish/fish/aliases.fish
+++ shells/fish/fish/aliases.fish
@@ -1,20 +1,20 @@
-alias getjh "/usr/libexec/java_home -v$1"
+abbr getjh "/usr/libexec/java_home -v$1"
 
-alias setJDK7        'set -g JAVA_HOME (getjh $JAVA7_VERSION)'
-alias setJDK8        'set -g JAVA_HOME (getjh $JAVA8_VERSION)'
-alias setJDK8_Oracle 'set -g JAVA_HOME (getjh $JAVA8_ORACLE_VERSION)'
-alias setJDK11       'set -g JAVA_HOME (getjh $JAVA11_VERSION)'
-alias setJDK13       'set -g JAVA_HOME (getjh $JAVA13_VERSION)'
+abbr setJDK7        'set -g JAVA_HOME (getjh $JAVA7_VERSION)'
+abbr setJDK8        'set -g JAVA_HOME (getjh $JAVA8_VERSION)'
+abbr setJDK8_Oracle 'set -g JAVA_HOME (getjh $JAVA8_ORACLE_VERSION)'
+abbr setJDK11       'set -g JAVA_HOME (getjh $JAVA11_VERSION)'
+abbr setJDK13       'set -g JAVA_HOME (getjh $JAVA13_VERSION)'
 
 # cleans mule runtime temp files and apps
-alias cm " \
+abbr cm " \
   rm ./logs/**/*.log 2>/dev/null; \
   rm ./logs/**/*.json 2>/dev/null; \
   rm -rf ./.mule/* 2>/dev/null; \
   rm -rf ./apps/* 2> /dev/null"
 
 # builds mule-uber extensions
-alias bme \
+abbr bme \
   "mvn clean install \
     -f mule-build-maven-plugins/pom.xml \
     -DskipTests \
@@ -24,33 +24,33 @@ alias bme \
     -DskipMunitTests"
 
 # builds mule-uber
-alias bm \
+abbr bm \
   "mvn clean install \
     -DskipTests \
     -DskipMunitTests \
     -T0.5C"
 
 # builds mule-uber and skips revapi validation
-alias bmn \
+abbr bmn \
   "mvn clean install \
     -DskipTests \
     -DskipMunitTests \
     -T0.5C \
     -Drevapi.skip"
 
-alias reset_license 'rm conf/muleLicenseKey.lic && touch conf/.lic-mule'
+abbr reset_license 'rm conf/muleLicenseKey.lic && touch conf/.lic-mule'
 
 # vim aliases
-alias v   'nvim'
-alias vi  'nvim'
-alias vim 'nvim'
+abbr v   'nvim'
+abbr vi  'nvim'
+abbr vim 'nvim'
 
 # fzf find files in current directory and edit the selected ones
-alias f 'v (fzf -i)'
+abbr f 'v (fzf -i)'
 
 switch (uname)
     case Linux
-      alias ls "ls --color=tty"
+      abbr ls "ls --color=tty"
     case Darwin
-      alias ls "ls -G"
+      abbr ls "ls -G"
 end
