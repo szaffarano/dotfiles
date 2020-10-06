91a416cd571d6ead670eb1695db1696cb410cc78
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2020-10-06 14:19:17 -0300

    Revert "Use abbr instead of alias"
    It's very annoying how abbr works in comparison with aliases.
    
    This reverts commit 9c1ba815756e236637ef9b6be2aa8fd3bd60353b.

--- shells/fish/fish/aliases.fish
+++ shells/fish/fish/aliases.fish
@@ -1,20 +1,20 @@
-abbr getjh "/usr/libexec/java_home -v$1"
+alias getjh "/usr/libexec/java_home -v$1"
 
-abbr setJDK7        'set -g JAVA_HOME (getjh $JAVA7_VERSION)'
-abbr setJDK8        'set -g JAVA_HOME (getjh $JAVA8_VERSION)'
-abbr setJDK8_Oracle 'set -g JAVA_HOME (getjh $JAVA8_ORACLE_VERSION)'
-abbr setJDK11       'set -g JAVA_HOME (getjh $JAVA11_VERSION)'
-abbr setJDK13       'set -g JAVA_HOME (getjh $JAVA13_VERSION)'
+alias setJDK7        'set -g JAVA_HOME (getjh $JAVA7_VERSION)'
+alias setJDK8        'set -g JAVA_HOME (getjh $JAVA8_VERSION)'
+alias setJDK8_Oracle 'set -g JAVA_HOME (getjh $JAVA8_ORACLE_VERSION)'
+alias setJDK11       'set -g JAVA_HOME (getjh $JAVA11_VERSION)'
+alias setJDK13       'set -g JAVA_HOME (getjh $JAVA13_VERSION)'
 
 # cleans mule runtime temp files and apps
-abbr cm " \
+alias cm " \
   rm ./logs/**/*.log 2>/dev/null; \
   rm ./logs/**/*.json 2>/dev/null; \
   rm -rf ./.mule/* 2>/dev/null; \
   rm -rf ./apps/* 2> /dev/null"
 
 # builds mule-uber extensions
-abbr bme \
+alias bme \
   "mvn clean install \
     -f mule-build-maven-plugins/pom.xml \
     -DskipTests \
@@ -24,33 +24,33 @@ abbr bme \
     -DskipMunitTests"
 
 # builds mule-uber
-abbr bm \
+alias bm \
   "mvn clean install \
     -DskipTests \
     -DskipMunitTests \
     -T0.5C"
 
 # builds mule-uber and skips revapi validation
-abbr bmn \
+alias bmn \
   "mvn clean install \
     -DskipTests \
     -DskipMunitTests \
     -T0.5C \
     -Drevapi.skip"
 
-abbr reset_license 'rm conf/muleLicenseKey.lic && touch conf/.lic-mule'
+alias reset_license 'rm conf/muleLicenseKey.lic && touch conf/.lic-mule'
 
 # vim aliases
-abbr v   'nvim'
-abbr vi  'nvim'
-abbr vim 'nvim'
+alias v   'nvim'
+alias vi  'nvim'
+alias vim 'nvim'
 
 # fzf find files in current directory and edit the selected ones
-abbr f 'v (fzf -i)'
+alias f 'v (fzf -i)'
 
 switch (uname)
     case Linux
-      abbr ls "ls --color=tty"
+      alias ls "ls --color=tty"
     case Darwin
-      abbr ls "ls -G"
+      alias ls "ls -G"
 end
