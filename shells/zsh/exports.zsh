802935b4f0dfa1804b8978446b8271d29298eaba
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2021-04-02 18:21:45 +0200

    Update PATH environment variable

--- shells/zsh/exports.zsh
+++ shells/zsh/exports.zsh
@@ -2,18 +2,9 @@ export EDITOR=nvim
 
 export GOPATH=$HOME/projects/go
 
-export RUST_VERSION=$(asdf current rust | awk '{ print $2 }')
-export PYTHON_VERSION=$(asdf current python | awk '{print $2}')
-
 PATH="$HOME/.local/bin:$PATH"
-PATH="$HOME/.local/anaconda3/bin:$PATH"
-PATH="$HOME/.local/bin:$PATH"
-PATH="$HOME/.local/Goneovim:$PATH"
 PATH="/usr/local/bin:$PATH"
 PATH="$GOPATH/bin:$PATH"
-PATH="$HOME/.local/exercism/:$PATH"
-PATH="$HOME/.asdf/installs/rust/$RUST_VERSION/bin:$PATH"
-PATH="$HOME/.asdf/installs/python/$PYTHON_VERSION/bin:$PATH"
 export PATH
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
