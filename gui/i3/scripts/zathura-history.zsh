527e210840f5213f70ffa22e0b60e34cf5e85833
Sebastián Zaffarano <sebastien.zaffarano@klarna.com>
2021-05-13 10:41:46 +0200

    Improve zathura history menu

---
+++ gui/i3/scripts/zathura-history.zsh
@@ -0,0 +1,31 @@
+#!/usr/bin/env zsh
+
+zathura_history="$HOME/.local/share/zathura/history"
+
+rofiopts=(
+   -dmenu
+   -p "Select PDF"
+   -i
+   -markup-rows
+   -lines 10
+   -bw 2 
+   -yoffset 2 
+)
+
+files=()
+
+cat $zathura_history | grep -Po '\[\K[^\]]*' | while read -r pdf
+do
+   if [ -f $pdf ]; then
+      files+=$pdf
+   fi
+done
+
+selected=$(print -l $files | rofi ${rofiopts[@]})
+
+# exit if nothing is selected
+[[ -z $selected ]] && exit
+
+zathura "$selected"
+
+exit 0
