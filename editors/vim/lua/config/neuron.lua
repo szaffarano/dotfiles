67e109842f69f6844a0dbc82ad392c147dd2bddc
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2021-07-04 18:20:43 +0200

    Tidy neuron configuration

--- editors/vim/lua/config/neuron.lua
+++ editors/vim/lua/config/neuron.lua
@@ -1,8 +1,7 @@
--- these are all the default values
 require'neuron'.setup {
     virtual_titles = true,
     mappings = true,
-    run = nil, -- function to run when in neuron dir
-    neuron_dir = '~/projects/zetteln', -- the directory of all of your notes, expanded by default (currently supports only one directory for notes, find a way to detect neuron.dhall to use any directory)
-    leader = "gz", -- the leader key to for all mappings, remember with 'go zettel'
+    run = nil,
+    neuron_dir = '~/projects/zetteln',
+    leader = "gz",
 }
