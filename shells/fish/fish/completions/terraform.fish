b146df1b4dbb4ab0935fe3a28a9787c35bf6b7d4
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2021-08-29 13:41:58 +0200

    Fix paths int terraform and kubectl autocomplete

--- shells/fish/fish/completions/terraform.fish
+++ shells/fish/fish/completions/terraform.fish
@@ -3,7 +3,7 @@ function __complete_terraform
     set -lx COMP_LINE (string join ' ' (commandline -o))
     test (commandline -ct) = ""
     and set COMP_LINE "$COMP_LINE "
-    /home/sebas/.asdf/installs/terraform/1.0.2/bin/terraform
+    terraform
 end
 complete -c terraform -a "(__complete_terraform)"
 
