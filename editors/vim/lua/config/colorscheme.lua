ea94ca7a32699e7adc805e0356214c02e6938f7e
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2021-10-06 19:13:53 +0200

    Configure gruvbox

--- editors/vim/lua/config/colorscheme.lua
+++ editors/vim/lua/config/colorscheme.lua
@@ -1,3 +1,10 @@
 vim.opt.termguicolors = true
 
 vim.cmd "colorscheme gruvbox-material"
+vim.cmd "set background=dark"
+vim.cmd 'let g:gruvbox_material_background = "medium"'
+vim.cmd "let g:gruvbox_material_enable_italic = 1"
+vim.cmd "let g:gruvbox_material_disable_italic_comment = 1"
+vim.cmd 'let g:gruvbox_material_ui_contrast = "hight"'
+vim.cmd 'let g:gruvbox_material_diagnostic_virtual_text = "colored"'
+vim.cmd 'let g:gruvbox_material_palette = "mix"'
