bb5bf49b6d1669c3f6e01c95fbd6d500fba5feb6
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2022-03-20 11:04:46 +0100

    Configure neovim global statusline

--- editors/vim/after/plugin/statusline.lua
+++ editors/vim/after/plugin/statusline.lua
@@ -4,8 +4,13 @@ if not ok then
   return
 end
 
+-- thanks TJ for this (https://www.youtube.com/watch?v=jH5PNvJIa6o)
+vim.cmd([[highlight WinSeparator guifg=#4e545c guibg=None]])
+
 lualine.setup({
   options = {
     theme = "material",
   },
 })
+
+vim.opt.laststatus = 3
