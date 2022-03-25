a339983f9127828e0d90d1719630877ea8d09796
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2022-03-25 08:47:52 +0100

    Disable neovim node, ruby and perl support

--- editors/vim/lua/sebas/disable_builtins.lua
+++ editors/vim/lua/sebas/disable_builtins.lua
@@ -15,6 +15,10 @@ vim.g.loaded_matchparen = 1
 vim.g.loaded_logiPat = 1
 vim.g.loaded_rrhelper = 1
 
+vim.g.loaded_node_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
+
 --vim.g.loaded_netrw = 1
 --vim.g.loaded_netrwPlugin = 1
 --vim.g.loaded_netrwSettings = 1
