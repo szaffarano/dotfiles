6270790cb46ed2202d3fb3d345a88c7388fdbbcb
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2022-04-19 08:16:52 +0200

    Add screen config

--- gui/i3/scripts/screen-mode-menu.zsh
+++ gui/i3/scripts/screen-mode-menu.zsh
@@ -20,6 +20,7 @@ display_modes_opts["Dell U27"]="$AUTORANDR DELL_U2721DE"
 display_modes_opts["Dell U27 HDMI"]="$AUTORANDR DELL_U2721DE_HDMI"
 display_modes_opts["Dell Laptop"]="$AUTORANDR DELL_LAPTOP"
 display_modes_opts["Dell Laptop Old"]="$AUTORANDR DELL_LAPTOP_OLD"
+display_modes_opts["Samsung"]="$AUTORANDR SAMSUNG"
 
 list=$(print -l ${(k)display_modes_opts} | sed s'/\"//g')
 action=$(echo $list | rofi ${rofi_opts[@]})
