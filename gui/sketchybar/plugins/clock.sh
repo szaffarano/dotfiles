198da75530e2363683334034b91f843bbd176504
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2022-10-21 15:08:32 +0000

    Add sketchybar config

---
+++ gui/sketchybar/plugins/clock.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+# The $NAME variable is passed from sketchybar and holds the name of
+# the item invoking this script:
+# https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
+
+sketchybar --set $NAME \
+  label="$(date '+%a, %b %d')  $(TZ=Europe/Stockholm date '+%H:%M') (STH) $(TZ=Europe/Helsinki date '+%H:%M') (HEL)"
+
